a95dc23f6d625b656eb5854d8ea716f8307a49e8
Goatkenziee <mackquisition@gmail.com>
2026-05-28 00:06:05 -0400

    Fix TypeScript and next.config.mjs errors

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,6 @@
-import { UserButton, auth } from "@clerk/nextjs";
+
+import { UserButton } from "@clerk/nextjs";
+import { auth } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
 
 export default function Home() {
